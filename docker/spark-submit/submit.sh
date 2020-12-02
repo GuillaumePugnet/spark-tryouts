d9169ff93dc108861a9ec827c24f347137ccb1c7
Guillaume Pugnet <guillaume.pugnet@avanade.com>
2020-12-02 14:10:25 +0100

    SPARK_MASTER_URL now built from NAME & PORT

--- docker/spark-submit/submit.sh
+++ docker/spark-submit/submit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
- 
+export SPARK_MASTER_URL=spark://${SPARK_MASTER_NAME}:${SPARK_MASTER_PORT}
+export SPARK_HOME=/spark
+
 /spark/bin/spark-submit \
 --class ${SPARK_APPLICATION_MAIN_CLASS} \
 --master ${SPARK_MASTER_URL} \
@@ -7,4 +9,4 @@
 --total-executor-cores 1 \
  ${SPARK_SUBMIT_ARGS} \
  ${SPARK_APPLICATION_JAR_LOCATION} \
- ${SPARK_APPLICATION_ARGS} \
+ ${SPARK_APPLICATION_ARGS}
